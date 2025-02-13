c0f196cacea73e6aa792951b8a20f6db4df4a1df
Otterdev_0341 <128258559+otterdev0341@users.noreply.github.com>
2025-02-14 06:37:33 +0700

    impl me api to check is token valid

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -1,61 +1,88 @@
 import axios from "axios";
 import { ILoginUserData, IRegisterUserData } from "../domain/AuthDto";
+import { Result, ResultUtils } from "../types/Result";
+import { METHODS } from "http";
 
-
-
-export default async function sign_up(registerData: IRegisterUserData): Promise<string> {
-
-    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-    const REGISTER_URL = import.meta.env.VITE_FEATURE_REGISTER;
-
-    try{
-        
-        if (!BASE_URL || !REGISTER_URL) {
-            throw new Error("API base URL or register URL is not defined in the environment variables.");
-            
-        }
-        console.log(registerData);
-        console.log(`${BASE_URL}${REGISTER_URL}`);
-        const response = await axios.post(`${BASE_URL}${REGISTER_URL}`, registerData,{
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        });
-        if (response.status === 201) {
-            return "user registered successfully";
-        } else if(response.status === 400) {
-            
-            return "Registration failed";
-        }
-    } catch (error){
-        console.error(error);
-        return "";
-    }
-    return "Unexpected error";
+export enum AuthError {
+    InvalidCredentials = "Invalid credentials",
+    UserAlreadyExists = "User already exists",
+    ServerError = "Server error",
+    UnknownError = "Unknown error",
 }
 
-
-export  async function sign_in(loginData: ILoginUserData): Promise<string> {
-    const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-    const LOGIN_URL = import.meta.env.VITE_FEATURE_LOGIN;
-
-    try{
-        if (!BASE_URL || !LOGIN_URL) {
-            throw new Error("API base URL or login URL is not defined in the environment variables.");
-        }
-        const response = await axios.post(`${BASE_URL}${LOGIN_URL}`, loginData,{
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        });
-        if (response.status === 200) {
-            return response.data;
-        } else if(response.status === 400) {
-            return "Login failed";
-        }
-    } catch (error){
-        console.error(error);
-        return "";
+export class AuthService{
+    
+    private base_url: string;
+    private feature_sign_up: string;
+    private feature_sign_in: string;
+    private feature_me: string;
+    
+    constructor() {
+        this.base_url = import.meta.env.VITE_API_BASE_URL;
+        this.feature_sign_up = import.meta.env.VITE_FEATURE_SIGN_UP;
+        this.feature_sign_in = import.meta.env.VITE_FEATURE_SIGN_IN;
+        this.feature_me = import.meta.env.VITE_FEATURE_ME;
     }
-    return "Unexpected error";
+
+    async sign_up(registerData: IRegisterUserData): Promise<Result<IRegisterUserData,string>> {
+        try {
+            const response = await fetch(`${this.base_url}${this.feature_sign_up}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(registerData)
+            });
+            if (!response.ok) {
+                return ResultUtils.Err(AuthError.ServerError);
+            }
+            return ResultUtils.Ok(registerData);
+        } catch (error) {
+            console.error(error);
+            return ResultUtils.Err(AuthError.UnknownError);
+        } 
+    }
+
+    async sign_in(loginData: ILoginUserData): Promise<Result<string,AuthError>> {
+        try {
+            const response = await fetch(`${this.base_url}${this.feature_sign_in}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(loginData)
+            });
+            if (!response.ok) {
+                return ResultUtils.Err(AuthError.InvalidCredentials);
+            }
+            const data = await response.json();
+            return ResultUtils.Ok(data.token as string);
+        } catch (error) {
+            console.error(error);
+            return ResultUtils.Err(AuthError.UnknownError);
+        }
+    }
+
+    // also can use to check is token valid
+    async me(token: string)
+        : Promise<Result<string,AuthError>> 
+    {
+        try {
+                const response = await fetch(`${this.base_url}${this.feature_me}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                });
+                if (!response.ok) {
+                    return ResultUtils.Err(AuthError.InvalidCredentials);
+                }
+                const data = await response.json();
+                return ResultUtils.Ok(data.token as string);
+            } catch (error) {
+                console.error(error);
+                return ResultUtils.Err(AuthError.UnknownError);
+            }
+    }
 }
